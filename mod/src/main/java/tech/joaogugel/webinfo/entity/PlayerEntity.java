019eada14e239dd79f0d81c9742b7cb098ad0e74
joao gugel <ojoaogugel@gmail.com>
2025-01-05 18:11:49 -0300

    feat: add extra player fields to the message

--- mod/src/main/java/tech/joaogugel/webinfo/entity/PlayerEntity.java
+++ mod/src/main/java/tech/joaogugel/webinfo/entity/PlayerEntity.java
@@ -4,22 +4,46 @@ import net.minecraft.world.phys.Vec3;
 
 public class PlayerEntity {
     public String dimensionName;
-    public String username;
+    public String playerName;
     public Vec3 worldPosition;
-    public float healthStatus;
-    public int foodStatus;
+    public float playerHealthLevel;
+    public int playerFoodLevel;
+    public int playerScore;
+    public boolean isInWater;
+    public boolean isOnFire;
+    public boolean isSleeping;
+    public boolean isAlive;
+    public boolean isRaining;
+    public boolean isThundering;
+    public boolean isClearWeather;
 
     public PlayerEntity(
             String dimensionName,
-            String username,
+            String playerName,
             Vec3 worldPosition,
-            float healthStatus,
-            int foodStatus
+            float playerHealthLevel,
+            int playerFoodLevel,
+            int playerScore,
+            boolean isInWater,
+            boolean isOnFire,
+            boolean isSleeping,
+            boolean isAlive,
+            boolean isRaining,
+            boolean isThundering,
+            boolean isClearWeather
     ) {
         this.dimensionName = dimensionName;
-        this.username = username;
+        this.playerName = playerName;
         this.worldPosition = worldPosition;
-        this.healthStatus = healthStatus;
-        this.foodStatus = foodStatus;
+        this.playerHealthLevel = playerHealthLevel;
+        this.playerFoodLevel = playerFoodLevel;
+        this.playerScore = playerScore;
+        this.isInWater = isInWater;
+        this.isOnFire = isOnFire;
+        this.isSleeping = isSleeping;
+        this.isAlive = isAlive;
+        this.isRaining = isRaining;
+        this.isThundering = isThundering;
+        this.isClearWeather = isClearWeather;
     }
 }
--- mod/src/main/java/tech/joaogugel/webinfo/event/EntityStatusEventHandler.java
+++ mod/src/main/java/tech/joaogugel/webinfo/event/EntityStatusEventHandler.java
@@ -4,7 +4,6 @@ package tech.joaogugel.webinfo.event;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -31,19 +30,36 @@ public class EntityStatusEventHandler {
         if (!entity.level().isClientSide && entity.getType() == EntityType.PLAYER) {
             Player player = (Player) entity;
 
-            float playerHealth = player.getHealth();
+            float playerHealthLevel = player.getHealth();
             Vec3 playerPosition = player.position();
-            int foodLevel = player.getFoodData().getFoodLevel();
+            int playerFoodLevel = player.getFoodData().getFoodLevel();
+            int playerScore = player.getScore();
             String playerName = player.getName().getString();
             String dimension = player.level().dimension().toString();
             String dimensionName = dimension.split(":")[2].replace("]", "");
+            boolean isInWater = player.isInWater();
+            boolean isOnFire = player.isOnFire();
+            boolean isSleeping = player.isSleeping();
+            boolean isAlive = player.isAlive();
+
+            boolean isRaining = player.level().isRaining();
+            boolean isThundering = player.level().isThundering();
+            boolean isClearWeather = !isRaining && !isThundering;
 
             PlayerEntity playerEntity = new PlayerEntity(
                     dimensionName,
                     playerName,
                     playerPosition,
-                    playerHealth,
-                    foodLevel
+                    playerHealthLevel,
+                    playerFoodLevel,
+                    playerScore,
+                    isInWater,
+                    isOnFire,
+                    isSleeping,
+                    isAlive,
+                    isRaining,
+                    isThundering,
+                    isClearWeather
             );
 
             eventMessageSender.sendPlayerEntity(playerEntity);
